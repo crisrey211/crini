5c3211d56a71879f32c07218aa2cc8ee5a856fbc
christian_reynaltt <creynaltt@maristak.net>
2025-02-05 02:16:09 +0100

    Fix: Marque cambiado de color fondo Alerta - texto centrado en movil

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -4,7 +4,7 @@ interface AlertProps {
 
 export default function Alert({ message }: AlertProps) {
     return (
-        <header className="bg-brand-900 h-9 w-full flex items-center justify-center text-white">
+        <header className="bg-brand-900 h-9 w-full flex items-center text-center justify-center text-white">
             <span className="text-xs font-medium">
                 {message}
             </span>
